7938446a043ffecf35491e633c10b1bff2233513
xiaohui12 <396761221@qq.com>
2017-12-02 22:58:46 +0800

    更新dao和service

--- OA/src/cn/bdqn/j25/service/TypeService.java
+++ OA/src/cn/bdqn/j25/service/TypeService.java
@@ -8,7 +8,9 @@ public interface TypeService {
 
 	public List<Types> findAll();//查询全部Types类信息
 	
+	public Types findByid(int id);//根据id查询
+	
 	public  Types addOrUpdateType(Types types); //增加或更新类型
 	
-	public void delType(Types types); //增加类型
+	public Boolean delType(Types types); //增加类型
 }
